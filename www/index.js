645a554c620271e744db35675d9173407b7bbfad
Johnny Hauser <johnnyhauser@gmail.com>
2015-09-07 20:48:27 -0500

    .

--- www/index.js
+++ www/index.js
@@ -1,11 +1,12 @@
 var host = 'a.com';
 
 document.addEventListener("deviceready", function() {
+  setTimeout(function() {
+    console.log(cordova.plugins);
+  }, 10000);
+  cordova.plugins.StatusBar.overlaysWebView(false);
+
   if (window.device.platform === 'iOS') {
-    setTimeout(function() {
-      console.log(cordova.plugins);
-    }, 10000);
-    cordova.plugins.StatusBar.overlaysWebView(false);
     alert('registering RTC globals!');
     cordova.plugins.iosrtc.registerGlobals();
     window.getUserMedia = navigator.getUserMedia.bind(navigator);
