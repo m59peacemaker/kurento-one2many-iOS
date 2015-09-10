da42f7c47682febd9a1935fd3bde8ee6d4d36cd9
Johnny Hauser <johnnyhauser@gmail.com>
2015-09-09 19:51:39 -0500

    .

--- www/js/index.js
+++ www/js/index.js
@@ -1,144 +1,152 @@
 var host = 'a.com'; // the domain or IP where the node server and kurento media server are running
 
-//document.addEventListener("deviceready", function() {
-  //if (window.device.platform === 'iOS') {
-    cordova && cordova.plugins.iosrtc.registerGlobals();
-    window.getUserMedia = navigator.getUserMedia.bind(navigator);
-  //}
-//});
-
-var ws = new WebSocket('ws://'+host+':8080/one2many');
-var video;
-var webRtcPeer;
-
-video = document.getElementById('video');
-
-document.getElementById('call').addEventListener('click', function() { presenter(); } );
-document.getElementById('viewer').addEventListener('click', function() { viewer(); } );
-document.getElementById('terminate').addEventListener('click', function() { stop(); } );
-
-window.onbeforeunload = function() {
-  ws.close();
+if (window.cordova) {
+  document.addEventListener("deviceready", function() {
+    if (window.device.platform === 'iOS') {
+      cordova && cordova.plugins.iosrtc.registerGlobals();
+      window.getUserMedia = navigator.getUserMedia.bind(navigator);
+    }
+    stuff();
+  });
+} else {
+  stuff();
 }
 
-ws.onmessage = function(message) {
-  var parsedMessage = JSON.parse(message.data);
+function stuff() {
 
-  switch (parsedMessage.id) {
-  case 'presenterResponse':
-    presenterResponse(parsedMessage);
-    break;
-  case 'viewerResponse':
-    viewerResponse(parsedMessage);
-    break;
-  case 'stopCommunication':
-    dispose();
-    break;
-  case 'iceCandidate':
-    webRtcPeer.addIceCandidate(parsedMessage.candidate)
-  default:
+  var ws = new WebSocket('ws://'+host+':8080/one2many');
+  var video;
+  var webRtcPeer;
 
+  video = document.getElementById('video');
+
+  document.getElementById('call').addEventListener('click', function() { presenter(); } );
+  document.getElementById('viewer').addEventListener('click', function() { viewer(); } );
+  document.getElementById('terminate').addEventListener('click', function() { stop(); } );
+
+  window.onbeforeunload = function() {
+    ws.close();
   }
-}
 
-function presenterResponse(message) {
-  if (message.response != 'accepted') {
-    var errorMsg = message.message ? message.message : 'Unknow error';
-    dispose();
-  } else {
-    webRtcPeer.processAnswer(message.sdpAnswer);
+  ws.onmessage = function(message) {
+    var parsedMessage = JSON.parse(message.data);
+
+    switch (parsedMessage.id) {
+    case 'presenterResponse':
+      presenterResponse(parsedMessage);
+      break;
+    case 'viewerResponse':
+      viewerResponse(parsedMessage);
+      break;
+    case 'stopCommunication':
+      dispose();
+      break;
+    case 'iceCandidate':
+      webRtcPeer.addIceCandidate(parsedMessage.candidate)
+    default:
+
+    }
   }
-}
 
-function viewerResponse(message) {
-  if (message.response != 'accepted') {
-    var errorMsg = message.message ? message.message : 'Unknow error';
-    dispose();
-  } else {
-    webRtcPeer.processAnswer(message.sdpAnswer);
+  function presenterResponse(message) {
+    if (message.response != 'accepted') {
+      var errorMsg = message.message ? message.message : 'Unknow error';
+      dispose();
+    } else {
+      webRtcPeer.processAnswer(message.sdpAnswer);
+    }
   }
-}
 
-function presenter() {
-  if (!webRtcPeer) {
+  function viewerResponse(message) {
+    if (message.response != 'accepted') {
+      var errorMsg = message.message ? message.message : 'Unknow error';
+      dispose();
+    } else {
+      webRtcPeer.processAnswer(message.sdpAnswer);
+    }
+  }
 
-    var options = {
-      localVideo: video,
-      onicecandidate : onIceCandidate
+  function presenter() {
+    if (!webRtcPeer) {
+
+      var options = {
+        localVideo: video,
+        onicecandidate : onIceCandidate
+        }
+
+      webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options, function(error) {
+        if(error) return onError(error);
+
+        this.generateOffer(onOfferPresenter);
+      });
+    }
+  }
+
+  function onOfferPresenter(error, offerSdp) {
+      if (error) return onError(error);
+
+    var message = {
+      id : 'presenter',
+      sdpOffer : offerSdp
+    };
+    sendMessage(message);
+  }
+
+  function viewer() {
+    if (!webRtcPeer) {
+
+      var options = {
+        remoteVideo: video,
+        onicecandidate : onIceCandidate
       }
 
-    webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options, function(error) {
-      if(error) return onError(error);
+      webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function(error) {
+        if(error) return onError(error);
 
-      this.generateOffer(onOfferPresenter);
-    });
-  }
-}
-
-function onOfferPresenter(error, offerSdp) {
-    if (error) return onError(error);
-
-  var message = {
-    id : 'presenter',
-    sdpOffer : offerSdp
-  };
-  sendMessage(message);
-}
-
-function viewer() {
-  if (!webRtcPeer) {
-
-    var options = {
-      remoteVideo: video,
-      onicecandidate : onIceCandidate
+        this.generateOffer(onOfferViewer);
+      });
     }
-
-    webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function(error) {
-      if(error) return onError(error);
-
-      this.generateOffer(onOfferViewer);
-    });
   }
-}
 
-function onOfferViewer(error, offerSdp) {
-  if (error) return onError(error)
+  function onOfferViewer(error, offerSdp) {
+    if (error) return onError(error)
 
-  var message = {
-    id : 'viewer',
-    sdpOffer : offerSdp
-  }
-  sendMessage(message);
-}
-
-function onIceCandidate(candidate) {
-  console.log('onIceCandidate');
-
-     var message = {
-        id : 'onIceCandidate',
-        candidate : candidate
-     }
-     sendMessage(message);
-}
-
-function stop() {
-  if (webRtcPeer) {
     var message = {
-        id : 'stop'
+      id : 'viewer',
+      sdpOffer : offerSdp
     }
     sendMessage(message);
-    dispose();
   }
-}
 
-function dispose() {
-  if (webRtcPeer) {
-    webRtcPeer.dispose();
-    webRtcPeer = null;
+  function onIceCandidate(candidate) {
+    console.log('onIceCandidate');
+
+       var message = {
+          id : 'onIceCandidate',
+          candidate : candidate
+       }
+       sendMessage(message);
   }
-}
 
-function sendMessage(message) {
-  var jsonMessage = JSON.stringify(message);
-  ws.send(jsonMessage);
+  function stop() {
+    if (webRtcPeer) {
+      var message = {
+          id : 'stop'
+      }
+      sendMessage(message);
+      dispose();
+    }
+  }
+
+  function dispose() {
+    if (webRtcPeer) {
+      webRtcPeer.dispose();
+      webRtcPeer = null;
+    }
+  }
+
+  function sendMessage(message) {
+    var jsonMessage = JSON.stringify(message);
+    ws.send(jsonMessage);
+  }
 }
